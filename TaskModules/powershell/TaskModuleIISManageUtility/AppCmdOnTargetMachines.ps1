330cae0730711c2bcadbd96a471ff77ccb34797b
Scott Heighway <scotlord80@gmail.com>
2025-02-03 08:51:07 +1300

    Missed another tool invoke.

--- TaskModules/powershell/TaskModuleIISManageUtility/AppCmdOnTargetMachines.ps1
+++ TaskModules/powershell/TaskModuleIISManageUtility/AppCmdOnTargetMachines.ps1
@@ -220,7 +220,7 @@ function Add-SslCert
     }
 
     Write-Verbose "Setting SslCert for website: $certCmd"
-    $result = Invoke-VstsTool -Filename "netsh" -Arguments $certCmd -RequireExitCodeZero
+    $result = Invoke-ToolInternal -Filename "netsh" -Arguments $certCmd -RequireExitCodeZero
     Write-Verbose "$certCmd executed with result: $result"
 }
 
